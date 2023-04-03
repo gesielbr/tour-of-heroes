6335555274f805e424635f30f33c6a668e60a591
gesielbr <gesiel.br@gmail.com>
2023-04-02 23:13:06 -0300

    Mat Table com filtro, Organização Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCardModule } from '@angular/material/card';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -28,15 +24,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
     AppRoutingModule,
-    MatIconModule,
+    BrowserAnimationsModule,
     FlexLayoutModule,
-    MatTooltipModule,
-    MatCardModule,
+    MaterialModule,
   ],
   //FormsModule para utilizar ([ngModel])
   providers: [],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { HEROES } from '../mock-heroes';
+
+export interface HeroesTable {
+  name: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-heroes',
@@ -13,10 +18,17 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   // selectedHero?: Hero;
 
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource = new MatTableDataSource(HEROES);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   constructor(
-    private heroService: HeroService
-  ) // private messageService: MessageService
-  {}
+    private heroService: HeroService // private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -26,78 +38,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe((heroes) => {
       this.heroes = heroes;
     });
-
-    /*  this.heroService.getHeroes().subscribe({
-      next(res) {
-        console.log('O retorno da API foi: ' + JSON.stringify(res, null, 2));
-      },
-      error(err) {
-        console.error('Algo errado aconteceu: ' + err);
-      },
-      complete() {
-        console.log('Consulta a API realizada com sucesso!');
-      },
-    }); */
-
-    /* this.heroService.getHeroes().subscribe(
-      function (res) {
-        console.log(res);
-      },
-      function (err) {
-        console.log(err);
-      },
-      function () {
-        console.log('Requisição concluída com sucesso');
-      }
-    ); */
-
-    /* this.heroService.getHeroes().subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (err) => {
-        console.log(err);
-      },
-      () => {
-        console.log('Requisição concluída com sucesso');
-      }
-    ); */
-
-    /*  this.heroService.getHeroes().subscribe(
-      (res) => console.log(res),
-      (err) => console.log(err),
-      () => console.log('Requisição concluída com sucesso')
-    ); */
-
-    /* const observable = new Observable((subscriber) => {
-      subscriber.next(1);
-      subscriber.next(2);
-      subscriber.next(3);
-      setTimeout(() => {
-        subscriber.next(4);
-        subscriber.complete();
-      }, 1000);
-    });
-
-    console.log('Antes do subscribe');
-
-    observable.subscribe({
-      next(res) {
-        console.log('Pegou o valor ' + res);
-      },
-      error(err) {
-        console.error('Algo errado aconteceu: ' + err);
-      },
-      complete() {
-        console.log('Realizado co sucesso');
-      },
-    });
-
-    console.log('Depois do subscribe'); */
   }
-
-  // onSelect(hero: Hero): void {
-  //   this.selectedHero = hero;
-  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  // }
 }
