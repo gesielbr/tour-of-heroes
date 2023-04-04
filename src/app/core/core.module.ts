b38d479d750db727b7c0b5eeb6fd6de670b886d3
gesielbr <gesiel.br@gmail.com>
2023-04-04 13:58:10 -0300

    Interface para usar as propriedades no @Input()

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 const COMPONENTS = [MessagesComponent, ToolbarComponent];
 
@@ -11,7 +12,15 @@ const MODULES = [FlexLayoutModule, MaterialModule];
 
 @NgModule({
   declarations: [COMPONENTS],
-  imports: [MODULES, RouterModule],
+  imports: [CommonModule, MODULES, RouterModule],
   exports: [COMPONENTS, MODULES],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule has already been loaded. Import this module in the AppModule.'
+      );
+    }
+  }
+}
