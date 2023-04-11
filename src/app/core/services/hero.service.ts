17ce5de9e24f8a8180cf576be07f177227caf311
gesielbr <gesiel.br@gmail.com>
2023-04-11 16:37:57 -0300

    refatoração método método this.messageService.add

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -3,22 +3,42 @@ import { Observable, of, throwError } from 'rxjs';
 import { Hero } from '../models/hero.model';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';
+
+  /* 
+  local
+  http://localhost:4200/heroes
+  http://localhost:3000/api/heroes 
+
+  frontend
+  http://heroes.vercel.app/heroes
+
+  backend produção exemplos
+  http://heroes.vercel.app/api/heroes
+  http://heroes.herokuapp.com/api/heroes
+  */
+
+  constructor(private messageService: MessageService, http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    this.log('fetched heroes');
     return heroes;
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find((hero) => hero.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
